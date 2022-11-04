e2abd7b99d497b772d6efd203d649fc4de434d19
Bayu Yasaputro <bayuwy@gmail.com>
2022-11-04 16:50:28 +0700

    add progresslabel to download cell

--- PodcastsApp/Modules/Download/DownloadViewCell.swift
+++ PodcastsApp/Modules/Download/DownloadViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DownloadViewCell: UITableViewCell {
     @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
